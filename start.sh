4fb35821d75690a0c996cba0013651b3933d9a91
Sam Jeston <sam.jeston@iohk.io>
2019-06-27 08:33:14 +1000

    Correct start scripts

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-sysStart="$(date +%s)" docker-compose up --build -d
+docker-compose --pull
+sysStart="$(date +%s)" docker-compose up -d
 echo "A cardano-sl demo cluster, http-bridge and cardano-wallet are now booting. Check the docker-compose logs in ~60 seconds"
--- wallet/wallet-start.sh
+++ wallet/wallet-start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "Waiting 50 seconds for Cardano SL to boot...."
 sleep 50
-cardano-wallet server --port 8080 --bridge-port 8090
+cardano-wallet serve --port 8080 --bridge-port 8090
